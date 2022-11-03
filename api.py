e3adb694c6f9ea33b39abd4efa9857d9f441d230
Raunak Mondal <raunakmondal07@gmail.com>
2022-11-03 15:21:41 -0700

    lesgoo

--- api.py
+++ api.py
@@ -62,7 +62,7 @@ class JokesAPI:
     
 if __name__ == "__main__": 
     # server = "http://127.0.0.1:5000" # run local
-    server = 'https://flask.nighthawkcodingsociety.com' # run from web
+    server = 'https://time.nighthawkcodingteams.cf' # run from web
     url = server + "/api/jokes"
     responses = []  # responses list
 
